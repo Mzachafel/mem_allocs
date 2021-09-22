a12f78636d4026113a5d555eca5174c69d03783f
Mzachafel <game148@yandex.ru>
2021-09-22 17:49:32 +0300

    Linear allocator update

--- linalloc/linalloc.c
+++ linalloc/linalloc.c
@@ -1,46 +1,36 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 struct linalloc {
 	void *start;
-	void *free;
-	void *end;
-	int align;
+	size_t offset;
+	size_t end;
 };
 
-struct linalloc *la_create(size_t size, size_t align)
+struct linalloc *la_create(size_t size)
 {
 	struct linalloc *la = malloc(sizeof(struct linalloc));
 	la->start = malloc(size);
-	la->free = la->start;
-	la->end = la->start+size;
-	la->align = align;
+	la->offset = 0;
+	la->end = size;
 	return la;
 }
 
 void *la_allocate(struct linalloc *la, size_t size)
 {
-	size += size % la->align;
-	if (la->free + size <= la->end) {
-		void *ret = la->free;
-		la->free += size;
-		return ret;
-	}
-	return NULL;
+	if (la->offset + size > la->end)
+		return NULL;
+	void *ret = la->free;
+	la->free += size;
+	return ret;
 }
 
 void la_free(struct linalloc *la)
 {
-	la->free = la->start;
+	la->offset = 0;
 }
 
 void la_destroy(struct linalloc *la)
 {
 	free(la->start);
 	free(la);
-}
-
-void la_debug(struct linalloc *la)
-{
-	printf("start: %p\nend:   %p\nfree:  %p\n", la->start, la->end, la->free);
 }
--- linalloc/linalloc.h
+++ linalloc/linalloc.h
@@ -1,10 +1,9 @@
 #ifndef LINALLOC_H
 #define LINALLOC_H
 
-struct linalloc *la_create(unsigned int size, unsigned int align);
+struct linalloc *la_create(unsigned int size);
 void *la_allocate(struct linalloc *la, unsigned int size);
 void la_free(struct linalloc *la);
 void la_destroy(struct linalloc *la);
-void la_debug(struct linalloc *la);
 
 #endif
